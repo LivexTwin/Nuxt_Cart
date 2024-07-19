fce294962df27dc21d4096c5932f1eee70727904
Anthony <anthonyjbmolina@gmail.com>
2024-07-19 13:57:32 -0400

    added modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  css: ["~/assets/css/style.css", "~/assets/css/main.css"],
+  modules: ["@pinia/nuxt", "@nuxt/icon"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=VT323&display=swap",
+        },
+      ],
+    },
+  },
+});
